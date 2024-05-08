4a7637b223027e57589561eb72c33bb4c2de5cfa
David Marsal Ivars <davidmarsalivars@gmail.com>
2024-05-08 13:51:45 -0400

    Add createGame function in GameController

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -2,9 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Game;
 use Illuminate\Http\Request;
 
 class GameController extends Controller
 {
-    //
+    public function createGame(Request $request)
+    {
+        $request->validate([
+            'user' => 'required|string',
+            'age' => 'required|integer',
+        ]);
+
+        $game = Game::create([
+            'user' => $request->user,
+            'age' => $request->age,
+            'secret_number' => $this->generateSecretNumber(),
+        ]);
+
+        return response()->json(['game_id' => $game->id], 201);
+    }
+
+    private function generateSecretNumber()
+    {
+    }
 }
