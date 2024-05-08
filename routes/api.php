c36c153b367e589b477b032f66ca666053e9425d
David Marsal Ivars <davidmarsalivars@gmail.com>
2024-05-08 19:14:19 -0400

    Add game routes

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,14 @@
 <?php
 
+use App\Http\Controllers\GameController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::prefix('game')->group(function () {
+    Route::post('/create', [GameController::class, 'createGame']);
+    Route::post('/{game_id}/propose', [GameController::class, 'proposeCombination']);
 });
