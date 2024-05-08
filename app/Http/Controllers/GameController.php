dab94cb8be27512021e9f3f3cbd1dc133267689f
David Marsal Ivars <davidmarsalivars@gmail.com>
2024-05-08 19:15:38 -0400

    Add proposeCombination and evaluateCombination function in GameController

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -26,4 +26,50 @@ class GameController extends Controller
         $max = (10 ** $digits) - 1;
         return strval(rand($min, $max));
     }
+
+    public function proposeCombination(Request $request, $game_id): \Illuminate\Http\JsonResponse
+    {
+        $request->validate([
+            'combination' => 'nullable|digits:4',
+        ]);
+
+        $game = Game::findOrFail($game_id);
+
+        if ($game->game_over) {
+            return response()->json(['message' => 'Game Over'], 400);
+        }
+
+        $evaluation = $this->evaluateCombination($game->secret_number, $request->combination);
+
+        if ($evaluation['toros'] === 4) {
+            $game->game_over = true;
+            $game->save();
+        }
+
+        return response()->json([
+            'combination' => $request->combination,
+            'toros' => $evaluation['toros'],
+            'vacas' => $evaluation['vacas'],
+        ]);
+    }
+
+    private function evaluateCombination($secret, $proposal)
+    {
+        $toros = 0;
+        $vacas = 0;
+
+        $secretArray = str_split($secret);
+        $proposalArray = str_split($proposal);
+
+        for ($i = 0; $i < 4; $i++) {
+            if ($secretArray[$i] === $proposalArray[$i]) {
+                $toros++;
+            } elseif (in_array($proposalArray[$i], $secretArray)) {
+                $vacas++;
+            }
+        }
+
+        return ['toros' => $toros, 'vacas' => $vacas];
+    }
+
 }
