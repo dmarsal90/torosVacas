d55b1c723723446d97f0b4e91551e46628dce24e
David Marsal Ivars <davidmarsalivars@gmail.com>
2024-05-09 16:57:41 -0400

    Add swagger documentation route

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\GameController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use L5Swagger\Http\Controllers\SwaggerController;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
@@ -14,3 +15,4 @@ Route::prefix('game')->group(function () {
     Route::delete('/{game_id}/deleteGame', [GameController::class, 'deleteGame']);
     Route::get('/{game_id}/previous-response/{attempt_number}', [GameController::class, 'getPreviousResponse']);
 });
+Route::get('/api/documentation', [SwaggerController::class, 'api'])->name('l5-swagger.api');
